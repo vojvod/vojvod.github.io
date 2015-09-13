87d954c4eb0451c9a9025b08e0a66504a4f9f3ff
Simeon Taskaris <simos.taskaris@gmail.com>
2015-09-13 21:53:09 +0300

    Update search.js

--- cmv/js/config/search.js
+++ cmv/js/config/search.js
@@ -1,12 +1,6 @@
 define({
 	map: true,
-    identifyLayerInfos: true,
-<<<<<<< HEAD
-    proxy_url: 'proxy/PHP/proxy.php',
+        identifyLayerInfos: true,
+        proxy_url: 'proxy/proxy.ashx',
 	GeometryService_url: 'http://sampleserver5.arcgisonline.com/arcgis/rest/services/Utilities/Geometry/GeometryServer'
 });
-=======
-    proxy_url: 'http://vojvod.github.io/cmv/proxy/proxy.ashx',
-	GeometryService_url: 'http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer'
-});
->>>>>>> origin/master
